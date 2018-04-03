579ffafda926889a130283db252926afe8d380e6
Ythotha <yaroslav.pon@gmail.com>
2018-04-03 20:59:13 +0300

    Заменил фиксированные значения на относительные, переместил файлы scss в папку css

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var	sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    return gulp.src('sass/**/style.scss')
+    return gulp.src('css/scss/**/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
         .pipe(gulp.dest('css/', ['object']));
