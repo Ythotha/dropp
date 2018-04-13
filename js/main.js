3363286fff5713b6158dee9c32f1ac467e0c5e30
Ythotha <yaroslav.pon@gmail.com>
2018-04-13 13:40:21 +0300

    first-commit

--- js/main.js
+++ js/main.js
@@ -3,4 +3,5 @@
 
     let hamburger = document.querySelector('.hamburger');
     hamburger.addEventListener('click', (event) => hamburger.classList.toggle('hamburger_activated'));
+    hamburger.addEventListener('click', (event) => document.querySelector('.header__list').classList.toggle('header__list_open'));
 })();
--- js/main1.js
+++ js/main1.js
@@ -1,12 +1,13 @@
 
-    document.jquerySelector('.hamburger').onclick = showMenu;
-    document.jquerySelector('.hamburger_activated').onclick = hideMenu;
+    document.querySelector('.hamburger').onclick = showMenu;
+    document.querySelector('.hamburger hamburger_activated').onclick = hideMenu;
 
     function showMenu() {
-        document.jquerySelector('.header__list').style.right = 0;
-        document.jquerySelector('.header__list').style.display = 'block';
+        document.querySelector('.header__list').style.right = 0;
+        document.querySelector('.header__list').style.display = 'block';
     }
 
     function hideMenu() {
-        document.jquerySelector('.header__list').style.right = '-100%';
+        document.querySelector('.header__list').style.right = '-100%';
+        document.querySelector('.header__list').style.display = 'none';
     }
